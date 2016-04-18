7d625daf9871e17115c421510129f8e36d600057
xflying777 <xflying777@yahoo.com.tw>
2016-04-18 16:49:00 +0800

    Cblas sample

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+ sudo apt-get install libblas-dev checkinstall
+ sudo apt-get install libblas-doc checkinstall
+ sudo apt-get install liblapack-dev checkinstall
+ sudo apt-get install liblapack-doc checkinstall
+
---
+++ sample.cpp
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "cblas.h"
+
+void initial(double *A, double *B, int N);
+double error(double *x, double *y, int N);
+void matrix_multiplication(double *A, double *B, double *C, int N);
+void dgemm_row(double *A, double *B, double *C, int N);
+void dgemm_col(double *A, double *B, double *C, int N);
+
+int main()
+{
+	printf("\n");
+	int N;
+	printf(" Input size N = ");
+	scanf("%d", &N);
+	printf(" N = %d \n\n", N);
+
+	double *A, *B, *C1, *C2, *exact;
+
+	A = (double *) malloc(N*N*sizeof(double));
+	B = (double *) malloc(N*N*sizeof(double));
+	C1 = (double *) malloc(N*N*sizeof(double));
+	C2 = (double *) malloc(N*N*sizeof(double));
+	exact = (double *) malloc(N*N*sizeof(double));
+
+	initial(A, B, N);
+
+	matrix_multiplication(A, B, exact, N);
+
+	dgemm_row(A, B, C1, N);
+
+	dgemm_col(A, B, C2, N);
+
+	printf(" Error of dgemm_row = %e \n", error(C1, exact, N*N));
+	printf(" Error of dgemm_col = %e \n", error(C2, exact, N*N));
+
+	return 0;
+}
+
+void initial(double *A, double *B, int N)
+{
+	int i;
+
+	// initail A
+	for (i=0; i<N*N; i++)	A[i] = 1.0*i;
+	// initial B
+	for (i=0; i<N*N; i++)	B[i] = 1.0*(i+1);
+}
+
+double error(double *x, double *y, int N)
+{
+	int i;
+	double e, temp;
+	e = 0.0;
+	for (i=0; i<N; i++)
+	{
+		temp = fabs(x[i] - y[i]);
+		if (temp > e)	e = temp;
+	}
+
+	return e;
+}
+
+// C = alpha * op(A) * op(B) + beta * op(C)
+void matrix_multiplication(double *A, double *B, double *C, int N)
+{
+	int i, j, k;
+	double alpha, beta, temp;
+
+	alpha = 1.0;
+	beta = 0.0;
+
+	for (i=0; i<N; i++)
+	{
+		for (j=0; j<N; j++)
+		{
+			temp = 0.0;
+			for (k=0; k<N; k++)
+			{
+				temp += A[N*i+k] * B[N*k+j];
+			}
+			C[N*i+j] = temp + beta * C[N*i+j];
+		}
+	}
+}
+
+// C = alpha * op(A) * op(B) + beta * op(C)
+void dgemm_row(double *A, double *B, double *C, int N)
+{
+	double alpha, beta;
+	alpha = 1.0;
+	beta = 0.0;
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, alpha, A, N, B, N, beta, C, N);
+}
+
+// C = alpha * op(A) * op(B) + beta * op(C)
+void dgemm_col(double *A, double *B, double *C, int N)
+{
+	double alpha, beta;
+	alpha = 1.0;
+	beta = 0.0;
+	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, N, N, N, alpha, B, N, A, N, beta, C, N);
+}
+
